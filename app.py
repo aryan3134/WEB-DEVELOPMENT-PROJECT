ac2c7ef70f4eab04abc16beaf6ff49555292cefc
aryan3134 <aryan3134.beai24@chitkara.edu.in>
2025-02-18 10:38:57 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -71,10 +71,5 @@ def dashboard():
     
     return redirect('/login')
 
-@app.route('/logout')
-def logout():
-    session.pop('email',None)
-    return redirect('/login')
-
 if __name__ == '__main__':
     app.run(debug=True)
